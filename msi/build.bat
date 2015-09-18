96f2bd4bb57bf304cdedb51b2f3d4a6cad3e4725
Jan Niklas Hasse <jhasse@gmail.com>
2015-09-18 18:05:18 +0200

    Add icon to uninstaller, let installer remove previous installed versions

--- msi/build.bat
+++ msi/build.bat
@@ -1,9 +1,28 @@
+@echo off
+
 SET DataDir=..\build\exe.win32-3.4
-move %DataDir%\sleeptimer.exe sleeptimer.exe
-"%WIX%bin\heat.exe" dir "%DataDir%" -cg DataFiles -var env.DataDir -gg -scom -sreg -sfrag -srd -dr data -out data.wxs || goto :error
-"%WIX%bin\candle.exe" main.wxs -ext WixUtilExtension || goto :error
-"%WIX%bin\candle.exe" data.wxs -ext WixUtilExtension || goto :error
-"%WIX%bin\light.exe" -out Sleeptimer.msi -ext WixUtilExtension -ext WixUIExtension main.wixobj data.wixobj || goto :error
+
+echo moving sleeptimer.exe out of build directory
+move %DataDir%\sleeptimer.exe sleeptimer.exe || goto :error
+echo.
+echo creating data.wxs
+"%WIX%bin\heat.exe" dir "%DataDir%" -nologo -cg DataFiles -var env.DataDir -gg -scom -sreg -sfrag -srd -dr data -out data.wxs || goto :error
+echo.
+echo candle
+"%WIX%bin\candle.exe" main.wxs -nologo -ext WixUtilExtension || goto :error
+echo.
+echo candle
+"%WIX%bin\candle.exe" data.wxs -nologo -ext WixUtilExtension || goto :error
+echo.
+echo creating Sleeptimer.msi
+"%WIX%bin\light.exe" -out Sleeptimer.msi -nologo -ext WixUtilExtension -ext WixUIExtension main.wixobj data.wixobj || goto :error
+
+goto finally
 
 :error
+echo Command failed!
+pause
+
+:finally
+echo Success!
 move sleeptimer.exe %DataDir%\sleeptimer.exe
