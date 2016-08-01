15c8b8373c83a1804a1576166eb902f58405bac2
Jan Niklas Hasse <jhasse@bixense.com>
2016-08-01 23:19:29 +0200

    Setup instructions for Windows

---
+++ README.md
@@ -0,0 +1,22 @@
+# Windows
+
+## Native Python
+
+Install [Visual Studio 2015](https://www.visualstudio.com/de-de/downloads/download-visual-studio-vs.aspx)
+with C++ support, [Python 3](https://www.python.org/downloads/) and [PyGObject for Windows](http://sourceforge.net/projects/pygobjectwin32/) with "Base packages" and "Gtk+".
+
+## MSYS2
+
+Open the Mingw64 shell and type:
+
+```
+pacman -S mingw-w64-x86_64-python3-gobject mingw-w64-x86_64-gtk3
+python3 main.py
+```
+
+To create a distributable exe file:
+
+```
+pacman -S mingw-w64-x86_64-python3-cx_Freeze
+python3 setup.py cx_Freeze
+```
--- setup.py
+++ setup.py
@@ -1,28 +1,35 @@
 from cx_Freeze import setup, Executable
 import os, site, sys
 
-include_dll_path = os.path.join(site.getsitepackages()[1], "gnome")
+if len(site.getsitepackages()) > 1:
+    include_dll_path = os.path.join(site.getsitepackages()[1], "gnome")
 
-missing_dll = [
-    'libgtk-3-0.dll',
-    'libgdk-3-0.dll',
-    'libatk-1.0-0.dll',
-    'libcairo-gobject-2.dll',
-    'libgdk_pixbuf-2.0-0.dll',
-    'libjpeg-8.dll',
-    'libpango-1.0-0.dll',
-    'libpangocairo-1.0-0.dll',
-    'libpangoft2-1.0-0.dll',
-    'libpangowin32-1.0-0.dll',
-    'libjasper-1.dll',
-    'librsvg-2-2.dll',
-    'libharfbuzz-0.dll',
-    'libtiff-5.dll',
-    'libwebp-5.dll',
-]
+    missing_dll = [
+        'libgtk-3-0.dll',
+        'libgdk-3-0.dll',
+        'libatk-1.0-0.dll',
+        'libcairo-gobject-2.dll',
+        'libgdk_pixbuf-2.0-0.dll',
+        'libjpeg-8.dll',
+        'libpango-1.0-0.dll',
+        'libpangocairo-1.0-0.dll',
+        'libpangoft2-1.0-0.dll',
+        'libpangowin32-1.0-0.dll',
+        'libjasper-1.dll',
+        'librsvg-2-2.dll',
+        'libharfbuzz-0.dll',
+        'libtiff-5.dll',
+        'libwebp-5.dll',
+    ]
 
-## We need to add all the libraries too (for themes, etc..)
-gtk_libs = ['etc', 'lib', 'share/icons']
+    ## We need to add all the libraries too (for themes, etc..)
+    gtk_libs = ['etc', 'lib', 'share/icons']
+else:
+    # MSYS2, not working yet
+    include_dll_path = 'C:\\msys64\\mingw64'
+    missing_dll = []
+    gtk_libs = []
+
 
 
 include_files = [(os.path.join(include_dll_path, x), x) for x in missing_dll]
